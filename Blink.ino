cd303ab410ad6916cdd35cdf4821330250359c5c
Elouan Bruzek <56214457+AloneDay-91@users.noreply.github.com>
2022-01-31 18:36:03 +0100

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,26 @@
+/*
+ Titre : blink+ elouan bruzek 119
+ faire clignoter les LEDS branchées sur les pattes 12 et 13 de la carte L'ARDUINO à une fréquence de 0,5Hz
+ */
+
+// Déclaration de variables globales
+
+int led=13; // numéro de la pin ou est connectée la led 13
+
+// déclaration des constantes
+
+// setup
+
+void setup() 
+{
+  pinMode(led,OUTPUT); // configure la pin 13 en sortie
+}
+
+// programme
+
+void loop() 
+{
+  digitalWrite(led,HIGH); // allume la led
+  delay(1000); // attendre 1s
+  digitalWrite(led,LOW); // allume la led
+}
